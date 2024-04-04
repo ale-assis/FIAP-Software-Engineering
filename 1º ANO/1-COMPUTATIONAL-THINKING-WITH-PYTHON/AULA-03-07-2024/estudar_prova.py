0bf492088e4963b7b22afe807605047c625838b8
Ale <alexandre7assis@gmail.com>
2024-04-04 13:52:04 -0300

    Create estudar_prova.py

---
+++ 1º ANO/1-COMPUTATIONAL-THINKING-WITH-PYTHON/AULA-03-07-2024/estudar_prova.py
@@ -0,0 +1,8 @@
+#ex1
+num1 = input('Digite um numero: ')
+num2 = input('Digite outro numero: ')
+
+if num1 > num2:
+    print(f'O maior número é {num1}')
+else: 
+    print(f'O maior número é {num2}')
